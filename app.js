cfab73e39022e3598b3bb21b17daf98a8284053b
Vyom Patel <patelvyom1000@gmail.com>
2026-07-28 00:17:49 +0530

    Error Handling Middleware

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello Haru from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello Haru from the middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -33,5 +33,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on the server!`,
+  });
+});
 //4) SERVER
 module.exports = app;
