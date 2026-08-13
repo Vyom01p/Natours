715f4f402175d0cbfa18f1e4d9ba23815f44afb8
Vyom Patel <patelvyom1000@gmail.com>
2026-08-13 21:28:02 +0530

    Prevention against NoSQL and XSS

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 //1) GLOBAL MIDDLEWARES
+//Set Security HTTP Headers
+app.use(helmet());
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+//Limit request from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from this IP, please try again in an hour',
 });
 app.use('/api', limiter);
-app.use(express.json());
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//Data Sanitization XSS
+app.use(xss());
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 // app.use((req, res, next) => {
 //   console.log('Hello Haru from the middleware');
