47d082e321bee72767ed23bf7f1531ce57a7cec4
Vyom Patel <patelvyom1000@gmail.com>
2026-08-20 22:55:09 +0530

    Pug set up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -11,7 +12,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 //1) GLOBAL MIDDLEWARES
+//Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 //Set Security HTTP Headers
 app.use(helmet());
 //Development logging
@@ -45,8 +51,7 @@ app.use(
     ],
   }),
 );
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
+
 // app.use((req, res, next) => {
 //   console.log('Hello Haru from the middleware');
 //   next();
@@ -69,6 +74,9 @@ app.use((req, res, next) => {
 //3) ROUTES
 
 // const userRouter = express.Router();
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
