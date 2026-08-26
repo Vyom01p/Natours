4923c79fd9a2d2e9bb6d60af4efd31571de755ef
Vyom Patel <patelvyom1000@gmail.com>
2026-08-26 22:43:17 +0530

    Payment Section

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -13,7 +13,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    mode: 'payment', // REQUIRED by modern Stripe API
+    mode: 'payment',
     line_items: [
       {
         price_data: {
