fd9521e1189a930138e13e63fc9c34c334182a2c
Vyom Patel <patelvyom1000@gmail.com>
2026-08-26 23:36:18 +0530

    Added New Booking section

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const catchAsync = require('../utils/catchAsync');
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get currently booked tour
@@ -9,7 +10,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session (Fixed typo: 'sessions' instead of 'session')
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -35,4 +36,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  //This is only temprorary
+  const { tour, user, price } = req.query;
+
+  if (!tour || !user || !price) return next();
+  await Booking.create({ tour, user, price });
+  res.redirect(req.originalUrl.split('?')[0]);
 });
